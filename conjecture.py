face3d9100aa354028493f73ecf06c29e3202dc2
jemo <reecejames934@gmail.com>
2022-04-20 21:19:12 +0300

    second commit, fixed some bugs

--- conjecture.py
+++ conjecture.py
@@ -1,12 +1,14 @@
 
-num  = int(input("Enter a number"))
+num  = int(input("Enter a number\n"))
 cycles = 0
 while True:
-    cycles +=1
+    cycles = cycles + 1
     print(num)
-    if (num %2==0):
-        num = num/2
-    elif num==1:
+    if num==1:
         break
+    elif num%2==0:
+        num = num / 2
     else:
-        mum = num * 3 + 1
+        num = num * 3 +1
+
+print("\n" + str(cycles))
